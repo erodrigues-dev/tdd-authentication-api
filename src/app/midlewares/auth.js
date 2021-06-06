d17891dff8081af6f7d9eccf9563b5254caa52ed
Elton Rodrigues <erodrigues.dev@gmail.com>
2021-06-06 19:49:29 -0300

    feat: return 401 if a invalid token is provided

--- src/app/midlewares/auth.js
+++ src/app/midlewares/auth.js
@@ -1,8 +1,21 @@
+const jwt = require("jsonwebtoken");
+const promisify = require("util");
+
 module.exports = (req, res, next) => {
-    const headerAuthorization = req.headers.authorization;
+  const headerAuthorization = req.headers.authorization;
 
-    if(!headerAuthorization)
-        return res.status(401).json({ message: 'No JWT token is provided' })
+  if (!headerAuthorization)
+    return res.status(401).json({ message: "No JWT token is provided" });
 
-    return next()
-}
+  try {
+    
+    const [, token] = headerAuthorization.split(" ");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid JWT token is provided' })
+  }
+
+
+};
